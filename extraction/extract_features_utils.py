f7967eaaeeed2d465d3f1f96cdc0dbd13203868d
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2024-08-22 17:55:19 +0100

    Update extract_features_utils.py
    just tidying up some loose ends

--- extraction/extract_features_utils.py
+++ extraction/extract_features_utils.py
@@ -12,21 +12,16 @@ def cepstral_mean_variance_normalization(mfcc):
 
 def extract_overlapping_mfcc(chunk, sr, num_mfcc, frame_length, hop_length):
     mfcc = librosa.feature.mfcc(y=chunk, sr=sr, n_mfcc=num_mfcc, n_fft=frame_length, hop_length=hop_length)
-    
-
     mfcc = cepstral_mean_variance_normalization(mfcc)
-    
     delta_mfcc = librosa.feature.delta(mfcc)
     delta2_mfcc = librosa.feature.delta(mfcc, order=2)
     combined_mfcc = np.vstack([mfcc, delta_mfcc, delta2_mfcc])
-
     return combined_mfcc
 
 def reduce_features(features):
     num_frames = features.shape[1]
     paired_frames = features[:, :num_frames // 2 * 2].reshape(features.shape[0], -1, 2)
     reduced_frames = paired_frames.mean(axis=2)
-    
     if num_frames % 2 == 1:
         last_frame = features[:, -1].reshape(-1, 1)
         reduced_final_features = np.hstack((reduced_frames, last_frame))
